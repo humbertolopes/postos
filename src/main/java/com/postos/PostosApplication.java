76f5d915e976bda095147e753366399a42cee3a2
humbertolopes <humberto.infor@gmail.com>
2018-06-02 02:01:12 -0300

    first commit

---
+++ src/main/java/com/postos/PostosApplication.java
@@ -0,0 +1,12 @@
+package com.postos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostosApplication.class, args);
+	}
+}
